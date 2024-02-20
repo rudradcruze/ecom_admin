e66a0650f2ded6d96b069656473ca9fa5b909918
rudradcruze <francisrudra@gmail.com>
2024-02-20 14:22:43 +0600

    add freezed library into product model

--- lib/pages/new_product_page.dart
+++ lib/pages/new_product_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class NewProductPage extends StatefulWidget {
   static const String routeName = '/newProduct';
+
   const NewProductPage({super.key});
 
   @override
@@ -12,7 +13,13 @@ class _NewProductPageState extends State<NewProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('New Product'),),
+      appBar: AppBar(
+        title: const Text('New Product'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16.0),
+        children: [],
+      ),
     );
   }
 }
